ee8e7fe7ac7d6075463a3db5e12507067c1f6b85
Szilágyi Dániel <szilagyi.daniel88@gmail.com>
2023-02-28 14:04:19 +0100

    Update UDPWeatherStationPlugin.cs
    Migrated fixes from [https://github.com/RotorsDev/RACPluginUDPWeatherStation](https://github.com/RotorsDev/RACPluginUDPWeatherStation)

--- Plugins/WeatherStation/UDPWeatherStationPlugin.cs
+++ Plugins/WeatherStation/UDPWeatherStationPlugin.cs
@@ -1,4 +1,4 @@
-﻿using MissionPlanner.Utilities;
+using MissionPlanner.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -21,6 +21,7 @@ namespace UDPWeatherStation
 {
     public class UDPWeatherStationPlugin : Plugin
     {
+        //private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         private int PORT;
         private IPEndPoint iPEndPoint;
@@ -28,16 +29,40 @@ namespace UDPWeatherStation
         private TimeSpan connectionTimeout;
         private DateTime lastWeatherUpdate;
 
-        private List<(string Name, int Index, double Offset, double Multiplier, string Unit)> tableConfig;
+        public class WeatherItem
+        {
+            public string Name { get; set; }
+            public double Value { get; set; }
+            public int Index { get; set; }
+            public double Offset { get; set; }
+            public double Multiplier { get; set; }
+            public string Unit { get; set; }
+            public bool Visible { get; set; }
+
+            public WeatherItem(string name, double value, int index, double offset, double multiplier, string unit, bool visible)
+            {
+                Name = name;
+                Value = value;
+                Index = index;
+                Offset = offset;
+                Multiplier = multiplier;
+                Unit = unit;
+                Visible = visible;
+            }
+            public WeatherItem(string name, int index, string unit, bool visible) : this(name, 0, index, 0, 1, unit, visible) {}
+            public WeatherItem(string name, int index, string unit) : this(name, index, unit, true) {}
+        }
+        public List<WeatherItem> tableConfig; // make it public so other plugins can access the data
         private Image imageOriginal;
         private bool neverConnected;
 
-        private string TuningConfigKey = "RACWeatherStationTuningValues";
+        private string ConfigSaveKey = "RACWeatherStationTuningValues";
         internal class TuningItem
         {
             public string Name { get; set; }
             public double Offset { get; set; }
             public double Multiplier { get; set; }
+            public bool Visible { get; set; }
         }
 
         private Label disconnectedLabel;
@@ -54,7 +79,7 @@ namespace UDPWeatherStation
 
         public override string Version
         {
-            get { return "0.1"; }
+            get { return "0.2.1"; }
         }
 
         public override string Author
@@ -65,7 +90,7 @@ namespace UDPWeatherStation
 
         //[DebuggerHidden]
         public override bool Init()
-		// Init called when the plugin dll is loaded
+        // Init called when the plugin dll is loaded
         {
             loopratehz = 1;  // Loop runs every second (The value is in Hertz, so 2 means every 500ms, 0.1f means every 10 second...)
             return true;	 // If it is false then plugin will not load
@@ -104,7 +129,7 @@ namespace UDPWeatherStation
                 disconnectedLabel.Width = flowPanel.ClientSize.Width;
                 disconnectedLabel.Padding = new Padding(5);
                 flowPanel.Controls.Add(disconnectedLabel);
-
+				
                 // Setup wind arrow picture
                 pBoxArrow = new PictureBox();
                 pBoxArrow.Name = "pictureBoxArrow";
@@ -117,28 +142,29 @@ namespace UDPWeatherStation
 
                 // Setup data table
                 // állomás iránya (nem kell) | szélsebesség 'm/s' | szélirány '°' | légnyomás 'miliBar' | belső hőmérséklet (nem kell) | páratartalom '%' | hőmérséklet '°C' | akksifesz 'Volt' |
-                tableConfig = new List<(string, int, double, double, string)>
+                tableConfig = new List<WeatherItem>
                 {
-                    //("Station heading", 0, 0.0, 1.0, "°"),
-                    ("Wind speed", 1, 0.0, 1.0, CurrentState.SpeedUnit),
-                    ("Wind direction", 2, 0.0, 1.0, "°"),
-                    ("Air pressure", 3, 0.0, 1.0, "mBar"),
-                    //("Internal temperature", 4, 0.0, 1.0, "°C"),
-                    ("Humidity", 5, 0.0, 1.0, "%"),
-                    ("External temperature", 6, 0.0, 1.0, "°C"),
-                    ("Battery voltage", 7, 0.0, 1.0, "V")
+                    new WeatherItem("Station heading", 0, "°", false) {},
+                    new WeatherItem("Wind speed", 1, CurrentState.SpeedUnit) {},
+                    new WeatherItem("Wind direction", 2, "°") {},
+                    new WeatherItem("Air pressure", 3, "mBar") {},
+                    new WeatherItem("Internal temperature", 4, "°C", false) {},
+                    new WeatherItem("Humidity", 5, "%") {},
+                    new WeatherItem("External temperature", 6, "°C") {},
+                    new WeatherItem("Battery voltage", 7, "V") {}
                 };
 
                 // Value multipliers
-                if (Settings.Instance.ContainsKey(TuningConfigKey))
+                if (Settings.Instance.ContainsKey(ConfigSaveKey))
                 {
                     // Load saved tuning values
-                    List<TuningItem> saved = Settings.Instance[TuningConfigKey].FromJSON<List<TuningItem>>();
+                    List<TuningItem> saved = Settings.Instance[ConfigSaveKey].FromJSON<List<TuningItem>>();
                     foreach (TuningItem savedItem in saved)
                     {
-                        (string Name, int Index, double Offset, double Multiplier, string Unit) tableItem = tableConfig.Where(tc => tc.Name == savedItem.Name).FirstOrDefault();
+                        WeatherItem tableItem = tableConfig.Where(tc => tc.Name == savedItem.Name).FirstOrDefault();
                         tableItem.Offset = savedItem.Offset;
                         tableItem.Multiplier = savedItem.Multiplier;
+                        tableItem.Visible = savedItem.Visible;
                     }
                 }
                 else
@@ -151,10 +177,11 @@ namespace UDPWeatherStation
                         {
                             Name = tableItem.Name,
                             Offset = tableItem.Offset,
-                            Multiplier = tableItem.Multiplier
+                            Multiplier = tableItem.Multiplier,
+                            Visible = tableItem.Visible
                         });
                     }
-                    Settings.Instance[TuningConfigKey] = toSave.ToJSON();
+                    Settings.Instance[ConfigSaveKey] = toSave.ToJSON();
                     Settings.Instance.Save();
                 }
 
@@ -162,27 +189,21 @@ namespace UDPWeatherStation
                 weatherTable = new TableLayoutPanel();
                 weatherTable.Name = "tableWeather";
                 weatherTable.ColumnCount = 2;
-                weatherTable.RowCount = tableConfig.Count + 1; // +1 row for time
-                for (int i = 0; i < weatherTable.RowCount; i++)
-                {
-                    // Column 0: Name
-                    Label l1 = new Label();
-                    l1.Text = i == 0 ? "Time" : tableConfig[i - 1].Name;
-                    l1.TextAlign = ContentAlignment.MiddleRight;
-                    l1.Padding = new Padding(5);
-                    l1.AutoSize = true;
-                    l1.Dock = DockStyle.Fill;
-                    weatherTable.Controls.Add(l1, 0, i);
+                weatherTable.RowCount = tableConfig.Count(x => x.Visible) + 1; // +1 row for time
 
-                    // Column 1: Value and unit
-                    Label l2 = new Label();
-                    l2.Text = i == 0 ? string.Empty : $"{Math.Round(0d, 2)} {tableConfig[i - 1].Unit}";
-                    l2.TextAlign = ContentAlignment.MiddleLeft;
-                    l2.Padding = new Padding(5);
-                    l2.AutoSize = true;
-                    l2.Dock = DockStyle.Fill;
-                    weatherTable.Controls.Add(l2, 1, i);
+                // Add time row
+                AddRow("Time", string.Empty);
+
+                // Add other rows
+                foreach (WeatherItem item in tableConfig)
+                {
+                    if (item.Visible)
+                    {
+                        AddRow(item.Name, item.Unit);
+                    }
                 }
+
+                // Format control
                 weatherTable.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
                 weatherTable.Width = flowPanel.ClientSize.Width;
                 for (int i = 0; i < weatherTable.ColumnCount; i++)
@@ -206,6 +227,37 @@ namespace UDPWeatherStation
             return true;     //If it is false plugin will not start (loop will not called)
         }
 
+        /// <summary>
+        /// Populates the next empty row in weatherTable
+        /// </summary>
+        /// <param name="name">Name to be displayed</param>
+        /// <param name="unit">Unit to be displayed</param>
+        private void AddRow(string name, string unit)
+        {
+            // Find next empty row
+            int i = 0;
+            while (i < weatherTable.RowCount && weatherTable.GetControlFromPosition(0, i) != null)
+                i++;
+
+            // Column 0: Name
+            Label l1 = new Label();
+            l1.Text = name;
+            l1.TextAlign = ContentAlignment.MiddleRight;
+            l1.Padding = new Padding(5);
+            l1.AutoSize = true;
+            l1.Dock = DockStyle.Fill;
+            weatherTable.Controls.Add(l1, 0, i);
+
+            // Column 1: Value and unit
+            Label l2 = new Label();
+            l2.Text = $"{Math.Round(0d, 2)} {unit}";
+            l2.TextAlign = ContentAlignment.MiddleLeft;
+            l2.Padding = new Padding(5);
+            l2.AutoSize = true;
+            l2.Dock = DockStyle.Fill;
+            weatherTable.Controls.Add(l2, 1, i);
+        }
+
         private void FlowPanel_Resize(object sender, EventArgs e)
         {
             MainV2.instance.BeginInvoke((MethodInvoker)(() =>
@@ -218,14 +270,10 @@ namespace UDPWeatherStation
         }
 
         public override bool Loop()
-		// Loop is called in regular intervalls (set by loopratehz)
+        // Loop is called in regular intervalls (set by loopratehz)
         {
             if (DateTime.Now.Subtract(lastWeatherUpdate) > connectionTimeout)
                 DisplayDisconnectedMessage();
-            // állomás iránya (nem kell) | szélsebesség 'm/s' | szélirány '°' | légnyomás 'miliBar' | belső hőmérséklet (nem kell) | páratartalom '%' | hőmérséklet '°C' | akksifesz 'Volt' |
-            //String message = "100 | 50 | 200 | 1234 | 15 | 45 | 5 | 11.2 |";
-            //DisplayMessage(message);
-
             return true;	//Return value is not used
         }
 
@@ -246,80 +294,99 @@ namespace UDPWeatherStation
             // Flip bool for first message
             neverConnected = false;
 
-            // Process message
+            // Log message
             Console.WriteLine($"UDP broadcast on port {PORT}: {message}");
-            LogMessage(message);
-            DisplayMessage(message);
+            //LogMessage(message);
+
+            // Update tableConfig values
+            UpdateData(message);
+
+            // Update UI
+            UpdateUI();
         }
 
         /// <summary>
         /// Log received UDP broadcast message
         /// </summary>
         /// <param name="message">Received UDP broadcast message</param>
-        private void LogMessage(string message)
-        {
-            // Log in csv format
-            //string logLine = $"\"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}\"";
-            //foreach (string item in message.Split('|'))
-            //    logLine += $",\"{item}\"";
-            //log.Info(logLine);
-        }
+        //private void LogMessage(string message)
+        //{
+        //    // Log in csv format
+        //    string logLine = $"\"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}\"";
+        //    foreach (string item in message.Split('|'))
+        //        logLine += $",\"{item}\"";
+        //    log.Info(logLine);
+        //}
 
         /// <summary>
-        /// Put received UDP broadcast message to the UI
+        /// Update the values in the weather data table
         /// </summary>
-        /// <param name="message">Received UDP broadcast message</param>
-        private void DisplayMessage(string message)
+        /// <param name="message">UDP message string</param>
+        private void UpdateData(string message)
         {
-            MainV2.instance.BeginInvoke((MethodInvoker)(() =>
+            //MainV2.instance.BeginInvoke((MethodInvoker)(() => // We have to access CurrentState, so...
+            //{
+            // message -> tableConfig
+            message = message.Replace('.', (0.1).ToString()[1])
+                             .Replace(',', (0.1).ToString()[1]); // suck it invariant culture!
+
+            foreach (WeatherItem item in tableConfig)
             {
-                message = message
-                .Replace('.', (0.1).ToString()[1])
-                .Replace(',', (0.1).ToString()[1]); // suck it invariant culture!
+                item.Value = double.Parse(message.Split('|')[item.Index]); // parse value from message
 
-                // Update table data
-                for (int i = 0; i < weatherTable.RowCount; i++)
+                MainV2.instance.BeginInvoke((MethodInvoker)(() => // TODO: can we get away with this?
                 {
-                    string content;
-                    if (i == 0) // Time
+                    if (item.Name.Contains("speed") && item.Unit != CurrentState.SpeedUnit) // Windspeed is a special case
                     {
-                        content = $"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}";
+                        item.Value *= CurrentState.multiplierspeed; // Apply set unit multiplier
+                        item.Unit = CurrentState.SpeedUnit; // Apply set unit text
                     }
-                    else if (tableConfig[i - 1].Name.Contains("speed")) // Wind speed
-                    {
-                        content = $"{Math.Round(double.Parse(message.Split('|')[tableConfig[i - 1].Index]) * CurrentState.multiplierspeed * tableConfig[i - 1].Multiplier + tableConfig[i - 1].Offset, 2)} {CurrentState.SpeedUnit}";
-                    }
-                    else // Everything else
-                    {
-                        content = $"{Math.Round(double.Parse(message.Split('|')[tableConfig[i - 1].Index]) * tableConfig[i - 1].Multiplier + tableConfig[i - 1].Offset, 2)} {tableConfig[i - 1].Unit}";
-                    }
-                    weatherTable.GetControlFromPosition(1, i).Text = content;
-                }
 
+                    item.Value *= item.Multiplier; // Apply multiplier
+                    item.Value += item.Offset; // Apply offset
 
-                for (int i = 1; i< weatherTable.RowCount; i++)
-                {
-                    double value = Math.Round(double.Parse(message.Split('|')[tableConfig[i - 1].Index]) * tableConfig[i - 1].Multiplier + tableConfig[i - 1].Offset, 2);
-                    switch (tableConfig[i - 1].Name)
+                    // Write values for protar
+                    switch (item.Name)
                     {
                         case "Wind speed":
-                            Host.cs.g_wind_vel = (float)value;
+                            Host.cs.g_wind_vel = (float)item.Value;
                             break;
                         case "Wind direction":
-                            Host.cs.g_wind_dir = (float)value;
+                            Host.cs.g_wind_dir = (float)item.Value;
                             break;
                         case "Air pressure":
-                            Host.cs.g_press = (float)value;
+                            Host.cs.g_press = (float)item.Value;
                             break;
                         case "Humidity":
-                            Host.cs.g_humidity = (float)value;
+                            Host.cs.g_humidity = (float)item.Value;
                             break;
                         case "External temperature":
-                            Host.cs.g_temp = (float)value;
+                            Host.cs.g_temp = (float)item.Value;
                             break;
                         default:
                             break;
                     }
+                }));
+            }
+            //}));
+        }
+
+        /// <summary>
+        /// Update the UI with the current values in the weather data table
+        /// </summary>
+        private void UpdateUI()
+        {
+            // tableConfig -> weatherTable
+            MainV2.instance.BeginInvoke((MethodInvoker)(() =>
+            {
+                // Update time row
+                weatherTable.GetControlFromPosition(1, 0).Text = $"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}";
+
+                // Update other rows
+                for (int i = 1; i < weatherTable.RowCount; i++) // Skip 1st row (time)
+                {
+                    WeatherItem item = tableConfig.Where(x => x.Name == weatherTable.GetControlFromPosition(0, i).Text).FirstOrDefault();
+                    weatherTable.GetControlFromPosition(1, i).Text = $"{item.Value} {item.Unit}";
                 }
 
                 // Update wind arrow
@@ -327,12 +394,10 @@ namespace UDPWeatherStation
                 rotatedBmp.SetResolution(imageOriginal.HorizontalResolution, imageOriginal.VerticalResolution);
                 Graphics graphics = Graphics.FromImage(rotatedBmp);
                 graphics.TranslateTransform(rotatedBmp.Width / 2, rotatedBmp.Height / 2);
-                int index = tableConfig.Where(item => item.Name.Contains("Wind direction")).FirstOrDefault().Index;
-                float newAngle = (float.Parse(message.Split('|')[index]) + 180) % 360; // Arrow points where the wind is blowing to
+                float newAngle = (float)tableConfig.Where(x => x.Name == "Wind direction").FirstOrDefault().Value; // Arrow points where the wind is blowing to
                 graphics.RotateTransform(newAngle);
                 graphics.TranslateTransform(-(rotatedBmp.Width / 2), -(rotatedBmp.Height / 2));
                 graphics.DrawImage(imageOriginal, new PointF(0, 0));
-                pBoxArrow.Image = rotatedBmp;
 
                 // Change control visibility & refresh
                 disconnectedLabel.Hide();
@@ -343,7 +408,7 @@ namespace UDPWeatherStation
         }
 
         /// <summary>
-        /// Dsiplay message on UI when connection to the weather station has been lost for the set time
+        /// Display message on UI when connection to the weather station has been lost for the set time
         /// </summary>
         private void DisplayDisconnectedMessage()
         {
@@ -368,7 +433,7 @@ namespace UDPWeatherStation
         }
 
         public override bool Exit()
-		// Exit called when plugin is terminated (usually when Mission Planner is exiting)
+        // Exit called when plugin is terminated (usually when Mission Planner is exiting)
         {
             return true;	// Return value is not used
         }
